027de30720b4ffe4b863783654af2abc573d53de
LucaaMueller <luca@acmueller.com>
2024-11-22 15:16:46 +0100

    Initial Commit

---
+++ src/main/java/org/example/web_eng2/WebEng2Application.java
@@ -0,0 +1,13 @@
+package org.example.web_eng2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEng2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEng2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/web_eng2/WebEng2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.web_eng2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEng2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
